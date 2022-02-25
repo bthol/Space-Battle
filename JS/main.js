92d426ab977968d438137e6bebe64148997f7d63
bthol <bthollaug@gmail.com>
2022-02-25 13:13:51 -0800

    testing selectors; forms vs buttons

--- JS/main.js
+++ JS/main.js
@@ -1,54 +1,94 @@
-const ussSchwarzeneggar = {
-    hull: 20,
+////////////////////////event listeners////////////////
+// document.querySelector(`#text1`).innerText = `${msg}`;
+let testElement = $(`#text1`).innerHtml = `${msg}`;
+let msg = "test text";
+console.log(testElement);
+document.querySelector(`#btn1`);
+
+
+
+
+
+/////////////////////data structures/////////////////////////////////
+const userShip = {
+    name: "user",
+    hull: 25,
     pulsebeam: {firepower: 2, accuracy: .6},
-    lazercannon: {firepower: 5, accuracy: .7}
+    lazercannon: {firepower: 5, accuracy: .75}
 };
 
 const aliens = [
     {name: "Fleetship", hull: 3, firepower: 2, accuracy: .6},
-    {name: "Tank", hull: 6, firepower: 2, accuracy: .6},
-    {name: "Sentinel", hull: 3, firepower: 2, accuracy: .8},
-    {name: "Mothership", hull: 6, firepower: 4, accuracy: .6}
+    {name: "Tanker", hull: 5, firepower: 1, accuracy: .6},
+    {name: "Sentinel", hull: 4, firepower: 2, accuracy: .8}
 ];
 
+const bosses = [
+    {name: "Commandship", hull: 10, firepower: 4, accuracy: .5},
+    {name: "Commandship", hull: 10, firepower: 4, accuracy: .5},
+    {name: "Mothership", hull: 20, firepower: 4, accuracy: .5}
+];
+
+///////////////////////GLOBAL VARIABLES////////////////////////////////
 let turn;
+
 let cannonCharge;
 let repairCharge;
+
 let currentShip;
 let enemiesDefeated;
+let bossCount = 0;
 
+/////////////////////////initilization/////////////////////////////////
 function init() {
-    ussSchwarzeneggar.hull = 20;
+    userShip.hull = 25;
     turn = 1;
     cannonCharge = 0;
     repairCharge = 0;
     enemiesDefeated = 0;
-    newEnemy();
-    playerMove();
+    bossCount = 0;
+    gameStart();
 };
 init();
 
+/////////////////////////ENEMY GENERATORS///////////////////////////////
 function newEnemy() {
-    currentShip = aliens[Math.floor(Math.random() * 3)];
+    currentShip = aliens[Math.floor(Math.random() * aliens.length)];
     alert(`A ${currentShip.name} approaches...`);
 };
 
 function bossEnemy() {
-    currentShip = aliens[3];
+    userShip.hull = 25;
+    currentShip = bosses[bossCount];
+    alert(`BOSS FIGHT!\n\n${currentShip.name} approaches...`);
+    bossCount += 1;
 };
 
-function turnHandler() {
-    turn = turn * -1;
-    if (turn === 1) {
+/////////////////////////GAME PAGE CONTROL//////////////////////////////////////
+function gameStart() {
+    let move = prompt(`Welcome to Space Battle.\n\nEnter ship name to start, or enter q to quit.`);
+    if (move === "q") {
+    } else {
+        userShip.name = move
+        newEnemy();
         playerMove();
-    } else if (turn === -1) {
-        alienMove();
     }
 };
 
+function gameOverMenu() {
+    let move = prompt(`DEFEAT!\n\nEnter 1 to start over.\nEnter 2 to exit game.`);
+    if (move === "1") {
+        init();
+    } else if (move === "2") {
+    } else {
+        alert("Invalid input!");
+    }
+};
+
+/////////////////////////////MOVE CONTROL///////////////////////////////////
 //fix the negative turns to charge displayed in prompt
 function playerMove() {
-    let move = prompt(`Enemy type: ${currentShip.name}\n\nAliens Obliterated = ${enemiesDefeated}\nHull integrity = ${ussSchwarzeneggar.hull}\n\nEnter '1' to use a pulsebeam attack\nEnter '2' to use a lazercannon attackLazer (${3 - cannonCharge} turns to charge.)\nEnter '3' to use repair (${10 - repairCharge} turns to charge.)`);
+    let move = prompt(`${userShip.name}\nEnemy type: ${currentShip.name}\n\nAliens Obliterated = ${enemiesDefeated}\nHull integrity = ${userShip.hull}\n\nEnter '1' to use a pulsebeam attack\nEnter '2' to use a lazercannon attackLazer (${3 - cannonCharge} turns to charge.)\nEnter '3' to use repair (${10 - repairCharge} turns to charge.)`);
     if (move === "1") {
         cannonCharge += 1;
         repairCharge += 1;
@@ -81,20 +121,34 @@ function alienMove() {
     alienAttack()
 };
 
+////////////////////////////////ALIEN MOVE///////////////////////////////////////
+function alienAttack() {
+    if (Math.random() < currentShip.accuracy) {
+        userShip.hull -= currentShip.firepower;
+        alert(`${currentShip.name} hit ${userShip.name}!`);
+        testDeath();
+    } else {
+        alert(`${currentShip.name} missed ${userShip.name}!`);
+        turnHandler();
+    }
+};
+
+////////////////////////////DEFENSIVE MOVES////////////////////////////////////////
 function repair() {
-    if (ussSchwarzeneggar.hull < 15) {
-        ussSchwarzeneggar.hull += 5;
+    if (userShip.hull < 20) {
+        userShip.hull += 5;
         alert('Ship repaired by 5!');
     } else {
-        ussSchwarzeneggar.hull += 20 - ussSchwarzeneggar.hull;
-        alert(`Ship repaired by ${20 - ussSchwarzeneggar.hull}!`);
+        alert(`Ship repaired by ${25 - userShip.hull}!`);
+        userShip.hull += 25 - userShip.hull;
     }
     turnHandler();
 };
 
+//////////////////////////////OFFENSIVE MOVES///////////////////////////////////////
 function pulsebeamAttack() {
-    if (Math.random() < ussSchwarzeneggar.pulsebeam.accuracy) {
-        currentShip.hull -= ussSchwarzeneggar.pulsebeam.firepower;
+    if (Math.random() < userShip.pulsebeam.accuracy) {
+        currentShip.hull -= userShip.pulsebeam.firepower;
         alert(`Pulsebeam attack hit the ${currentShip.name}!`);
         testDeath(1);
     } else {
@@ -104,8 +158,8 @@ function pulsebeamAttack() {
 };
 
 function lazercannonAttack() {
-    if (Math.random() < ussSchwarzeneggar.pulsebeam.accuracy) {
-        currentShip.hull -= ussSchwarzeneggar.lazercannon.firepower;
+    if (Math.random() < userShip.pulsebeam.accuracy) {
+        currentShip.hull -= userShip.lazercannon.firepower;
         alert(`Lazercannon attack hit ${currentShip.name}!`);
         testDeath(1);
     } else {
@@ -114,34 +168,18 @@ function lazercannonAttack() {
     }
 };
 
-function alienAttack() {
-    if (Math.random() < currentShip.accuracy) {
-        ussSchwarzeneggar.hull -= currentShip.firepower;
-        alert(`${currentShip.name} hit the USS Schwarzeneggar!`);
-        testDeath();
-    } else {
-        alert(`${currentShip.name} missed USS Schwarzeneggar!`);
-        turnHandler();
-    }
-};
-
+//////////////////////////////////TESTS///////////////////////////////////////////
 function testDeath (x) {
     if (x === 1) {
         if (currentShip.hull <= 0) {
             enemiesDefeated += 1;
             alert("Enemy ship was obliterated!");
-            if (enemiesDefeated === 25) {
-                bossEnemy();
-                playerMove();
-            } else {
-            newEnemy();
-            playerMove();
-            }
+            bossTest();
         } else {
             turnHandler();
         }
     } else {
-        if (ussSchwarzeneggar.hull <= 0) {
+        if (userShip.hull <= 0) {
             gameOverMenu();
         } else {
             turnHandler();
@@ -149,12 +187,19 @@ function testDeath (x) {
     }
 };
 
-function gameOverMenu() {
-    let move = prompt(`DEFEAT!\n\nEnter 1 to start over.\nEnter 2 to exit game.`);
-    if (move === "1") {
-        init();
-    } else if (move === "2") {
-    } else {
-        alert("Invalid input!");
+function bossTest() {
+    if (enemiesDefeated === 25 || enemiesDefeated === 50 || enemiesDefeated === 75) {
+        bossEnemy();
+        playerMove();
+    }
+}
+
+function turnHandler() {
+    turn = turn * -1;
+    if (turn === 1) {
+        playerMove();
+    } else if (turn === -1) {
+        alienMove();
     }
 };
+
