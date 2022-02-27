85e1e332928391d94aba6c107e7296e39c2f9e0d
bthol <bthollaug@gmail.com>
2022-02-27 13:50:37 -0800

    many changes

--- JS/main.js
+++ JS/main.js
@@ -1,17 +1,16 @@
-////////////////////////event listeners////////////////
-// document.querySelector(`#text1`).innerText = `${msg}`;
-let testElement = $(`#text1`).innerHtml = `${msg}`;
-let msg = "test text";
-console.log(testElement);
-document.querySelector(`#btn1`);
+///////////////////////GLOBAL VARIABLES////////////////////////////////
+let page;
 
+let cannonCharge = 0;
+let repairCharge = 0;
 
-
-
+let currentShip;
+let enemiesDefeated = 0;
+let bossCount = 0;
 
 /////////////////////data structures/////////////////////////////////
 const userShip = {
-    name: "user",
+    name: "User1",
     hull: 25,
     pulsebeam: {firepower: 2, accuracy: .6},
     lazercannon: {firepower: 5, accuracy: .75}
@@ -26,30 +25,71 @@ const aliens = [
 const bosses = [
     {name: "Commandship", hull: 10, firepower: 4, accuracy: .5},
     {name: "Commandship", hull: 10, firepower: 4, accuracy: .5},
+    {name: "Commandship", hull: 10, firepower: 4, accuracy: .5},
     {name: "Mothership", hull: 20, firepower: 4, accuracy: .5}
 ];
 
-///////////////////////GLOBAL VARIABLES////////////////////////////////
-let turn;
+////////////////////////DOM Element Selectors////////////////////////
+const btnEl1 = document.querySelector(`#btn1`);
+const btnEl2 = document.querySelector(`#btn2`);
+const btnEl3 = document.querySelector(`#btn3`);
+const promptEl = document.querySelector(`#prompt`);
+const alertEl = document.querySelector(`#alert`);
 
-let cannonCharge;
-let repairCharge;
-
-let currentShip;
-let enemiesDefeated;
-let bossCount = 0;
+////////////////////////Event Listeners///////////////////////////////
+btnEl1.addEventListener('click', buttonTester1)
+btnEl2.addEventListener('click', buttonTester2)
+btnEl3.addEventListener('click', buttonTester3)
 
 /////////////////////////initilization/////////////////////////////////
 function init() {
     userShip.hull = 25;
-    turn = 1;
+    turn = -1;
+    page = 0;
     cannonCharge = 0;
     repairCharge = 0;
     enemiesDefeated = 0;
     bossCount = 0;
-    gameStart();
+    btnEl1.innerText = `default btn1`;
+    btnEl2.innerText = `default btn2`;
+    btnEl3.innerText = `default btn3`;
+    promptEl.innerText = `default prompt`;
+    alertEl.innerText = `default alert`;
+    mainMenu();
 };
 init();
+
+/////////////////////////GAME PAGE//////////////////////////////////////////////
+function mainMenu() {
+    page = 0;
+    alertEl.innerText = `Welcome to the Space Battle main menu.`;
+    btnEl1.innerText = `Play`;
+    btnEl2.innerText = `Restart`;
+    btnEl3.innerText = `Quit`;
+    promptEl.innerText = ``;
+};
+
+function playerMove() {
+    page = 1;
+    alertEl.innerText = ``;
+    btnEl1.innerText = `Pulsebeam`;
+    btnEl2.innerText = `Lazercannon`;
+    btnEl3.innerText = `Repair`;
+    alertEl.innerText = `${currentShip.name}`;
+    promptEl.innerText = `User name: ${userShip.name}\nHull integrity = ${userShip.hull}\n\nAliens Obliterated = ${enemiesDefeated}\n\nLazercannon: ${3 - cannonCharge} turns to charge.\nRepair: ${10 - repairCharge} turns to charge.`;
+};
+
+function gameOverMenu() {
+    page = 2;
+    let move = prompt(`DEFEAT!\n\nEnter 1 to start over.\nEnter 2 to exit game.`);
+    if (move === "1") {
+        init();
+    } else if (move === "2") {
+    } else {
+        alert("Invalid input!");
+        gameOverMenu();
+    }
+};
 
 /////////////////////////ENEMY GENERATORS///////////////////////////////
 function newEnemy() {
@@ -64,63 +104,6 @@ function bossEnemy() {
     bossCount += 1;
 };
 
-/////////////////////////GAME PAGE CONTROL//////////////////////////////////////
-function gameStart() {
-    let move = prompt(`Welcome to Space Battle.\n\nEnter ship name to start, or enter q to quit.`);
-    if (move === "q") {
-    } else {
-        userShip.name = move
-        newEnemy();
-        playerMove();
-    }
-};
-
-function gameOverMenu() {
-    let move = prompt(`DEFEAT!\n\nEnter 1 to start over.\nEnter 2 to exit game.`);
-    if (move === "1") {
-        init();
-    } else if (move === "2") {
-    } else {
-        alert("Invalid input!");
-    }
-};
-
-/////////////////////////////MOVE CONTROL///////////////////////////////////
-//fix the negative turns to charge displayed in prompt
-function playerMove() {
-    let move = prompt(`${userShip.name}\nEnemy type: ${currentShip.name}\n\nAliens Obliterated = ${enemiesDefeated}\nHull integrity = ${userShip.hull}\n\nEnter '1' to use a pulsebeam attack\nEnter '2' to use a lazercannon attackLazer (${3 - cannonCharge} turns to charge.)\nEnter '3' to use repair (${10 - repairCharge} turns to charge.)`);
-    if (move === "1") {
-        cannonCharge += 1;
-        repairCharge += 1;
-        pulsebeamAttack();
-    } else if (move === '2') {
-        if (cannonCharge >= 3) {
-            repairCharge += 1;
-            cannonCharge = 0;
-            lazercannonAttack();
-        } else {
-            alert(`lazercannon needs ${3 - cannonCharge} turns to charge.`);
-            playerMove();
-        }
-    } else if (move === '3') {
-        if (repairCharge >= 10) {
-            cannonCharge += 1;
-            repairCharge = 0;
-            repair();
-        } else {
-            alert(`repair needs ${10 - repairCharge} turns to charge`)
-            playerMove();
-        }
-    } else {
-        alert("Invalid input!");
-        playerMove();
-    }
-};
-
-function alienMove() {
-    alienAttack()
-};
-
 ////////////////////////////////ALIEN MOVE///////////////////////////////////////
 function alienAttack() {
     if (Math.random() < currentShip.accuracy) {
@@ -129,42 +112,56 @@ function alienAttack() {
         testDeath();
     } else {
         alert(`${currentShip.name} missed ${userShip.name}!`);
-        turnHandler();
     }
 };
 
 ////////////////////////////DEFENSIVE MOVES////////////////////////////////////////
 function repair() {
-    if (userShip.hull < 20) {
-        userShip.hull += 5;
-        alert('Ship repaired by 5!');
+    if (repairCharge >= 10) {
+        cannonCharge += 1;
+        repairCharge = 0;
+        if (userShip.hull < 20) {
+            userShip.hull += 5;
+            alert('Ship repaired by 5!');
+        } else {
+            alert(`Ship repaired by ${25 - userShip.hull}!`);
+            userShip.hull += 25 - userShip.hull;
+        }
+        alienAttack();
     } else {
-        alert(`Ship repaired by ${25 - userShip.hull}!`);
-        userShip.hull += 25 - userShip.hull;
+        alert(`repair: ${10 - repairCharge} turns to charge`)
     }
-    turnHandler();
 };
 
 //////////////////////////////OFFENSIVE MOVES///////////////////////////////////////
 function pulsebeamAttack() {
+    cannonCharge += 1;
+    repairCharge += 1;
     if (Math.random() < userShip.pulsebeam.accuracy) {
         currentShip.hull -= userShip.pulsebeam.firepower;
         alert(`Pulsebeam attack hit the ${currentShip.name}!`);
         testDeath(1);
     } else {
         alert(`Pulsebeam attack missed the ${currentShip.name}!`);
-        turnHandler();
+        alienAttack();
     }
 };
 
 function lazercannonAttack() {
-    if (Math.random() < userShip.pulsebeam.accuracy) {
-        currentShip.hull -= userShip.lazercannon.firepower;
-        alert(`Lazercannon attack hit ${currentShip.name}!`);
-        testDeath(1);
+    if (cannonCharge >= 3) {
+        repairCharge += 1;
+        cannonCharge = 0;
+        if (Math.random() < userShip.pulsebeam.accuracy) {
+            currentShip.hull -= userShip.lazercannon.firepower;
+            alert(`Lazercannon attack hit ${currentShip.name}!`);
+            testDeath(1);
+        } else {
+            alert(`Lazercannon attack missed ${currentShip.name}!`);
+            alienAttack();
+        }
     } else {
-        alert(`Lazercannon attack missed ${currentShip.name}!`);
-        turnHandler();
+        alert(`lazercannon needs ${3 - cannonCharge} turns to charge.`);
+        playerMove();
     }
 };
 
@@ -176,30 +173,45 @@ function testDeath (x) {
             alert("Enemy ship was obliterated!");
             bossTest();
         } else {
-            turnHandler();
+            alienAttack();
         }
     } else {
         if (userShip.hull <= 0) {
             gameOverMenu();
-        } else {
-            turnHandler();
         }
     }
 };
 
 function bossTest() {
-    if (enemiesDefeated === 25 || enemiesDefeated === 50 || enemiesDefeated === 75) {
+    if (enemiesDefeated === 25 || enemiesDefeated === 50 || enemiesDefeated === 75 || enemiesDefeated === 100) {
         bossEnemy();
         playerMove();
     }
 }
 
-function turnHandler() {
-    turn = turn * -1;
-    if (turn === 1) {
+function buttonTester1() {
+    if (page === 0) {
+        newEnemy();
         playerMove();
-    } else if (turn === -1) {
-        alienMove();
+    } else if (page === 1) {
+        pulsebeamAttack();
+    } else {
+    }
+};
+function buttonTester2() {
+    if (page === 0) {
+        init();
+    } else if (page = 1) {
+        lazercannonAttack();
+    } else {
+    }
+};
+function buttonTester3() {
+    if (page === 0) {
+        alertEl.innerText = `The game has been quit.`;
+    } else if (page === 1) {
+        repair();
+    } else {
     }
 };
 
