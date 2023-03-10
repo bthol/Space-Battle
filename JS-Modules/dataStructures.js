274456661b8a03055c1443a8e14d7af82d43a6ec
bthol <95544562+bthol@users.noreply.github.com>
2023-03-10 15:37:01 -0800

    better data structure

--- JS-Modules/dataStructures.js
+++ JS-Modules/dataStructures.js
@@ -1,24 +1,25 @@
 const user = {
     name: "User",
     score: 0,
-    health: 200,
-    shield: 50,
+    health: {level: 200, maxLevel: 200, minLevel: 0},
+    repair: {rechargeAmmount: 5, rechargeTime: 10},
+    shield: {level: 50, maxLevel: 50, minLevel: 0, rechargeAmmount: 5, rechargeTime: 5},
     pulsebeam: {damage: 20, accuracy: .6},
     lazercannon: {baseDamage: 40, baseCharge: 3, overDamage: 80, overCharge: 10, accuracy: .8}
 };
 
 const aliens = [
-    {name: "Subort", health: 20, score: 100, damage: 10, accuracy: .4},
-    {name: "Tankbug", health: 70, score: 300, damage: 20, accuracy: .4},
-    {name: "Sentinel", health: 60, score: 200, damage: 15, accuracy: .8}
+    {name: "Subort", maxHealth: 20, score: 100, damage: 10, accuracy: .4},
+    {name: "Tankbug", maxHealth: 70, score: 300, damage: 20, accuracy: .4},
+    {name: "Sentinel", maxHealth: 60, score: 200, damage: 15, accuracy: .8}
 ];
 
 const bosses = [
-    {name: "Morphuos", health: 100, score: 2000, damage: 30, accuracy: .3},
-    {name: "Grotek", health: 130, score: 2000, damage: 50, accuracy: .2},
-    {name: "Mantlebrot", health: 150, score: 2000, damage: 45, accuracy: .4},
-    {name: "Harvester", health: 200, score: 2000, damage: 40, accuracy: .4},
-    {name: "Liminal", health: 250, score: 5000, damage: 60, accuracy: .4}
+    {name: "Morphuos", maxHealth: 100, score: 2000, damage: 30, accuracy: .3},
+    {name: "Grotek", maxHealth: 130, score: 2000, damage: 50, accuracy: .2},
+    {name: "Mantlebrot", maxHealth: 150, score: 2000, damage: 45, accuracy: .4},
+    {name: "Harvester", maxHealth: 200, score: 2000, damage: 40, accuracy: .4},
+    {name: "Liminal", maxHealth: 250, score: 5000, damage: 60, accuracy: .4}
 ];
 
 const bossRound = [15];
