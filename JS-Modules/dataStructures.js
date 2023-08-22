ea7296171fdbca1bc5655f9341ff6eac33115054
Blake <bthollaug@gmail.com>
2023-08-22 00:43:47 -0700

    stat adjust

--- JS-Modules/dataStructures.js
+++ JS-Modules/dataStructures.js
@@ -5,7 +5,7 @@ const user = {
     repair: {rechargeAmmount: 50, rechargeTime: 10},
     shield: {level: 50, maxLevel: 50, minLevel: 0, rechargeAmmount: 50, rechargeTime: 5},
     pulsebeam: {damage: 20, accuracy: .6},
-    lazercannon: {baseDamage: 40, baseCharge: 3, overDamage: 80, overCharge: 10, accuracy: .8}
+    lazercannon: {baseDamage: 40, baseCharge: 5, overDamage: 80, overCharge: 10, accuracy: 1}
 };
 
 const aliens = [
@@ -18,8 +18,8 @@ const bosses = [
     {name: "Morphuos", maxHealth: 100, score: 2000, damage: 30, accuracy: .3},
     {name: "Grotek", maxHealth: 130, score: 2000, damage: 50, accuracy: .2},
     {name: "Mantlebrot", maxHealth: 150, score: 2000, damage: 45, accuracy: .4},
-    {name: "Harvester", maxHealth: 200, score: 2000, damage: 40, accuracy: .4},
-    {name: "Liminal", maxHealth: 250, score: 5000, damage: 60, accuracy: .4}
+    {name: "Harvester", maxHealth: 200, score: 5000, damage: 40, accuracy: .4},
+    {name: "Liminal", maxHealth: 250, score: 10000, damage: 60, accuracy: .4}
 ];
 
 const bossRound = [10, 20, 30, 40, 50];
