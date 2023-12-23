1f60cf43b5fc0f73a0c7dbf5ffe1447d63136b71
bthol <95544562+bthol@users.noreply.github.com>
2023-12-23 11:11:06 -0800

    style + stats

--- JS-Modules/dataStructures.js
+++ JS-Modules/dataStructures.js
@@ -5,7 +5,7 @@ const user = {
     repair: {rechargeAmmount: 50, rechargeTime: 10},
     shield: {level: 50, maxLevel: 50, minLevel: 0, rechargeAmmount: 50, rechargeTime: 5},
     pulsebeam: {damage: 20, accuracy: .6},
-    lazercannon: {baseDamage: 30, baseCharge: 5, overDamage: 50, overCharge: 10, accuracy: 1}
+    lazercannon: {baseDamage: 30, baseCharge: 5, overDamage: 50, overCharge: 3, accuracy: 1}
 };
 
 const aliens = [
@@ -22,7 +22,7 @@ const bosses = [
     {name: "Liminal", maxHealth: 250, score: 20000, damage: 60, accuracy: .4}
 ];
 
-const bossRound = [2, 20, 30, 40, 50];
+const bossRound = [10, 20, 30, 40, 50];
 
 export {
     user,
